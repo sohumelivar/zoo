a3af07133f2bfe7c9caacc79750dd7fb21987a78
Kirill <kirpitch@inbox.ru>
2022-11-08 16:08:05 +0300

    Main page buttons and links to others page

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -4,7 +4,16 @@ const Layout = require('./Layout');
 module.exports = function MainPage({ title }) {
   return (
     <Layout title={title}>
-      <h3>Main page</h3>
+      <nav className="navMenu">
+        <a href=""><button type="button" className="btn btn-outline-dark">Наши животные</button></a>
+        {' '}
+        <a href="/tariffs"><button type="button" className="btn btn-outline-dark">Тарифы и услуги</button></a>
+        {' '}
+        <a href=""><button type="button" className="btn btn-outline-dark">Вход для администратора</button></a>
+      </nav>
+      <div className="container">
+        <h3>Добро пожаловать в урюпинский Зоопарк!</h3>
+      </div>
     </Layout>
   );
 };
