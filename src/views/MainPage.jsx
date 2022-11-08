85e4648612b7c34fa16f1918f688285b6205bc1e
Kirill <kirpitch@inbox.ru>
2022-11-08 21:09:05 +0300

    added elephant

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -4,6 +4,7 @@ const Layout = require('./Layout');
 module.exports = function MainPage({ title, username }) {
   return (
     <Layout username={username} title={title}>
+      <link rel="stylesheet" href="/css/animals.css" />
       <script defer src="js/adminLogin.js" />
       <div className="container">
         <h3>Добро пожаловать в урюпинский Зоопарк!</h3>
@@ -20,6 +21,9 @@ module.exports = function MainPage({ title, username }) {
           <button type="submit" className="btn btn-info entry">Войти</button>
           <p id="errLog"> </p>
         </form>
+      </div>
+      <div className="elephant body">
+        <div className="face" />
       </div>
     </Layout>
   );
