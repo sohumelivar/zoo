7ae8477e487b9e4beb903f1a31b4b40c0ac3cea3
Kirill <kirpitch@inbox.ru>
2022-11-08 12:22:23 +0300

    Added links change tariffs and animals from Admin page

--- src/views/AdminPage.jsx
+++ src/views/AdminPage.jsx
@@ -4,7 +4,12 @@ const Layout = require('./Layout');
 module.exports = function AdminPage({ title }) {
   return (
     <Layout title={title}>
-      Hello, admin
+      <h3>Hello admin</h3>
+      <div className="container">
+        <a href=""><button type="button" className="btn btn-warning">Изменить тарифы</button></a>
+        {' '}
+        <a href=""><button type="button" className="btn btn-warning">Изменить животных</button></a>
+      </div>
     </Layout>
   );
 };
